57a93a0906cdf23946fbf5ad36bdc920692ea696
Ankurdewangan12 <ankurdewangan20004@gmail.com>
2025-08-01 14:21:49 +0530

    data_ingestion file build and moduler coding work

---
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+'''TO create pipeline = flow of work'''
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier import logger
+
+'''continue form here'''
+try:
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion=DataIngestion(config =data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.extract_zip_file()
+except Exception as e:
+    raise e
+
