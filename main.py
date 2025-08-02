e5391673e9e93a447c92227136aab591f1c6a313
Ankurdewangan12 <ankurdewangan20004@gmail.com>
2025-08-02 13:06:43 +0530

    Data ingestion completed

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from src.cnnClassifier import logger
-logger.info("welcome to check logger")
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    obj=DataIngestionTraningPipeline()
+    obj.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -3,13 +3,25 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
 
-'''continue form here'''
-try:
-    config = ConfigurationManager()
-    data_ingestion_config = config.get_data_ingestion_config()
-    data_ingestion=DataIngestion(config =data_ingestion_config)
-    data_ingestion.download_file()
-    data_ingestion.extract_zip_file()
-except Exception as e:
-    raise e
+STAGE_NAME = " Data Ingestion Stage"
+
+class DataIngestionTraningPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion=DataIngestion(config =data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        obj=DataIngestionTraningPipeline()
+        obj.main()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
